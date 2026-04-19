be49bc5dd40936dcae890535b0b27d1f88b8af20
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-19 20:45:40 +0530

    DAY 4

--- database/d3.sql
+++ database/d3.sql
@@ -1,9 +1,9 @@
-SELECT id, COUNT(*) AS num
-FROM (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id AS id FROM RequestAccepted
-) t
-GROUP BY id
-ORDER BY num DESC
-LIMIT 1;
+# Write your MySQL query statement below
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
