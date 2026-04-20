b7efb5abc2521ff954e8e872066216b84bba5ed9
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-20 22:37:57 +0530

    DAY 5

---
+++ Hashmap/Index.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function (s, t) {
+  if (s.length !== t.length) return false;
+
+  let mapST = new Map();
+  let mapTS = new Map();
+
+  for (let i = 0; i < s.length; i++) {
+    let charS = s[i];
+    let charT = t[i];
+
+    if (mapST.has(charS)) {
+      if (mapST.get(charS) !== charT) return false;
+    } else {
+      mapST.set(charS, charT);
+    }
+
+    if (mapTS.has(charT)) {
+      if (mapTS.get(charT) !== charS) return false;
+    } else {
+      mapTS.set(charT, charS);
+    }
+  }
+
+  return true;
+};
