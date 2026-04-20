6343e37f4f64067d69ff00cec15f39d310005791
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-20 22:46:20 +0530

    DAY 5

--- Hashmap/Index.js
+++ Hashmap/Index.js
@@ -1,40 +1,26 @@
 /**
- * @param {number[][]} moves
- * @return {string}
+ * @param {number[]} groupSizes
+ * @return {number[][]}
  */
-var tictactoe = function (moves) {
-  let board = Array.from({ length: 3 }, () => Array(3).fill(""));
+var groupThePeople = function (groupSizes) {
+  let map = new Map();
+  let result = [];
 
-  // fill board
-  for (let i = 0; i < moves.length; i++) {
-    let [r, c] = moves[i];
-    board[r][c] = i % 2 === 0 ? "X" : "O";
+  for (let i = 0; i < groupSizes.length; i++) {
+    let size = groupSizes[i];
+
+    if (!map.has(size)) {
+      map.set(size, []);
+    }
+
+    map.get(size).push(i);
+
+    // if group is full → push to result
+    if (map.get(size).length === size) {
+      result.push([...map.get(size)]);
+      map.set(size, []); // reset for next group
+    }
   }
 
-  const check = (ch) => {
-    // rows & cols
-    for (let i = 0; i < 3; i++) {
-      if (
-        (board[i][0] === ch && board[i][1] === ch && board[i][2] === ch) ||
-        (board[0][i] === ch && board[1][i] === ch && board[2][i] === ch)
-      ) {
-        return true;
-      }
-    }
-
-    // diagonals
-    if (
-      (board[0][0] === ch && board[1][1] === ch && board[2][2] === ch) ||
-      (board[0][2] === ch && board[1][1] === ch && board[2][0] === ch)
-    ) {
-      return true;
-    }
-
-    return false;
-  };
-
-  if (check("X")) return "A";
-  if (check("O")) return "B";
-
-  return moves.length === 9 ? "Draw" : "Pending";
+  return result;
 };
