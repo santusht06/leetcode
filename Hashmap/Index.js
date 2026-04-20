e0790505796188d9fa5e3d003c6560cbfa869c53
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-20 22:39:34 +0530

    DAY 5

--- Hashmap/Index.js
+++ Hashmap/Index.js
@@ -1,24 +1,43 @@
 /**
  * @param {number[]} nums
- * @param {number} k
- * @return {boolean}
+ * @return {number[]}
  */
-var containsNearbyDuplicate = function (nums, k) {
-  let set = new Set();
+var majorityElement = function (nums) {
+  let count1 = 0,
+    count2 = 0;
+  let candidate1 = null,
+    candidate2 = null;
 
-  for (let i = 0; i < nums.length; i++) {
-    // if duplicate found in window
-    if (set.has(nums[i])) {
-      return true;
-    }
-
-    set.add(nums[i]);
-
-    // maintain window size k
-    if (set.size > k) {
-      set.delete(nums[i - k]);
+  for (let num of nums) {
+    if (num === candidate1) {
+      count1++;
+    } else if (num === candidate2) {
+      count2++;
+    } else if (count1 === 0) {
+      candidate1 = num;
+      count1 = 1;
+    } else if (count2 === 0) {
+      candidate2 = num;
+      count2 = 1;
+    } else {
+      count1--;
+      count2--;
     }
   }
 
-  return false;
+  count1 = 0;
+  count2 = 0;
+
+  for (let num of nums) {
+    if (num === candidate1) count1++;
+    else if (num === candidate2) count2++;
+  }
+
+  let result = [];
+  let n = nums.length;
+
+  if (count1 > Math.floor(n / 3)) result.push(candidate1);
+  if (count2 > Math.floor(n / 3)) result.push(candidate2);
+
+  return result;
 };
