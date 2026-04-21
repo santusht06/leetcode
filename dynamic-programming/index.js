c4606fa1ffbd15ca8d4c67caf4d18cefcd833416
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-21 21:51:05 +0530

    DAY 6

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,28 +1,64 @@
 /**
- * @param {string} s
- * @param {string} t
- * @return {number}
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {string[][]}
  */
-var numDistinct = function (s, t) {
-  let m = s.length,
-    n = t.length;
+var findLadders = function (beginWord, endWord, wordList) {
+  let wordSet = new Set(wordList);
+  if (!wordSet.has(endWord)) return [];
 
-  let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+  let graph = new Map();
+  let level = new Map();
+  let queue = [beginWord];
 
-  // base case
-  for (let i = 0; i <= m; i++) {
-    dp[i][0] = 1;
-  }
+  level.set(beginWord, 0);
 
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (s[i - 1] === t[j - 1]) {
-        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-      } else {
-        dp[i][j] = dp[i - 1][j];
+  // BFS
+  while (queue.length) {
+    let word = queue.shift();
+    let currLevel = level.get(word);
+
+    for (let i = 0; i < word.length; i++) {
+      for (let c = 97; c <= 122; c++) {
+        let next =
+          word.slice(0, i) + String.fromCharCode(c) + word.slice(i + 1);
+
+        if (wordSet.has(next)) {
+          if (!level.has(next)) {
+            level.set(next, currLevel + 1);
+            queue.push(next);
+          }
+
+          // only connect shortest path edges
+          if (level.get(next) === currLevel + 1) {
+            if (!graph.has(word)) graph.set(word, []);
+            graph.get(word).push(next);
+          }
+        }
       }
     }
   }
 
-  return dp[m][n];
+  let res = [];
+
+  // DFS to build paths
+  const dfs = (word, path) => {
+    if (word === endWord) {
+      res.push([...path]);
+      return;
+    }
+
+    if (!graph.has(word)) return;
+
+    for (let next of graph.get(word)) {
+      path.push(next);
+      dfs(next, path);
+      path.pop();
+    }
+  };
+
+  dfs(beginWord, [beginWord]);
+
+  return res;
 };
