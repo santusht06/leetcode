cdc5bfb6abc1786d8c0476c0c932a4da2e295655
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-21 21:45:44 +0530

    DAY 6

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,16 +1,33 @@
 /**
- * @param {number} m
- * @param {number} n
+ * @param {string} word1
+ * @param {string} word2
  * @return {number}
  */
-var uniquePaths = function (m, n) {
-  let dp = Array.from({ length: m }, () => Array(n).fill(1));
+var minDistance = function (word1, word2) {
+  let m = word1.length;
+  let n = word2.length;
 
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+  let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+
+  // base cases
+  for (let i = 0; i <= m; i++) dp[i][0] = i;
+  for (let j = 0; j <= n; j++) dp[0][j] = j;
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] =
+          1 +
+          Math.min(
+            dp[i - 1][j], // delete
+            dp[i][j - 1], // insert
+            dp[i - 1][j - 1], // replace
+          );
+      }
     }
   }
 
-  return dp[m - 1][n - 1];
+  return dp[m][n];
 };
