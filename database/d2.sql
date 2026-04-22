ea95d8ea8795ff918dc7581caf888f755835a785
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-22 22:12:45 +0530

    DAY 7

--- database/d2.sql
+++ database/d2.sql
@@ -1,11 +1,16 @@
 SELECT 
-    ROUND(
-        100 * SUM(order_date = customer_pref_delivery_date) / COUNT(*),
-        2
-    ) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id, order_date) IN (
-    SELECT customer_id, MIN(order_date)
-    FROM Delivery
-    GROUP BY customer_id
-);
+    id,
+    SUM(CASE WHEN month = 'Jan' THEN revenue END) AS Jan_Revenue,
+    SUM(CASE WHEN month = 'Feb' THEN revenue END) AS Feb_Revenue,
+    SUM(CASE WHEN month = 'Mar' THEN revenue END) AS Mar_Revenue,
+    SUM(CASE WHEN month = 'Apr' THEN revenue END) AS Apr_Revenue,
+    SUM(CASE WHEN month = 'May' THEN revenue END) AS May_Revenue,
+    SUM(CASE WHEN month = 'Jun' THEN revenue END) AS Jun_Revenue,
+    SUM(CASE WHEN month = 'Jul' THEN revenue END) AS Jul_Revenue,
+    SUM(CASE WHEN month = 'Aug' THEN revenue END) AS Aug_Revenue,
+    SUM(CASE WHEN month = 'Sep' THEN revenue END) AS Sep_Revenue,
+    SUM(CASE WHEN month = 'Oct' THEN revenue END) AS Oct_Revenue,
+    SUM(CASE WHEN month = 'Nov' THEN revenue END) AS Nov_Revenue,
+    SUM(CASE WHEN month = 'Dec' THEN revenue END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
