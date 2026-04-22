1c6087950619ae60589af894e5294d2edbc47d54
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-22 22:05:49 +0530

    DAY 7

--- database/d2.sql
+++ database/d2.sql
@@ -1,9 +1,5 @@
-SELECT 
-    CASE
-        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id + 1
-        WHEN id % 2 = 0 THEN id - 1
-        ELSE id
-    END AS id,
-    student
-FROM Seat
-ORDER BY id;
+UPDATE Salary
+SET sex = CASE
+    WHEN sex = 'm' THEN 'f'
+    ELSE 'm'
+END;
