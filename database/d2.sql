85f46bfc7b348fbf87836b7c0f50dfc6b7cd793a
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-22 22:05:13 +0530

    DAY 7

--- database/d2.sql
+++ database/d2.sql
@@ -1,5 +1,9 @@
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+SELECT 
+    CASE
+        WHEN id % 2 = 1 AND id != (SELECT MAX(id) FROM Seat) THEN id + 1
+        WHEN id % 2 = 0 THEN id - 1
+        ELSE id
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
