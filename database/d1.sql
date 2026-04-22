70f530a91bb99fcdf51ca7109af81c09fa33bf85
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-22 22:15:03 +0530

    DAY 7

--- database/d1.sql
+++ database/d1.sql
@@ -1,5 +1,12 @@
-SELECT id, movie, description, rating
-FROM Cinema
-WHERE id % 2 = 1
-  AND description != 'boring'
-ORDER BY rating DESC;
+SELECT 
+    s.student_id,
+    s.student_name,
+    sub.subject_name,
+    COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+    ON s.student_id = e.student_id
+    AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name;
