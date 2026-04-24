9c2d2ddc48f1e2c5c542381c4389825212aa4833
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-24 22:26:08 +0530

    DAY 9

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,59 +1,38 @@
 /**
- * @param {number[]} nums
- * @param {number} goal
+ * @param {number} x
+ * @param {number} target
  * @return {number}
  */
-var minAbsDifference = function (nums, goal) {
-  let n = nums.length;
-  let mid = Math.floor(n / 2);
+var leastOpsExpressTarget = function (x, target) {
+  let memo = new Map();
 
-  let left = nums.slice(0, mid);
-  let right = nums.slice(mid);
+  const dfs = (t) => {
+    if (memo.has(t)) return memo.get(t);
 
-  const getSums = (arr) => {
-    let res = [];
-    let m = arr.length;
+    if (t === 0) return 0;
+    if (t === 1) return 1; // x/x
 
-    for (let mask = 0; mask < 1 << m; mask++) {
-      let sum = 0;
-      for (let i = 0; i < m; i++) {
-        if (mask & (1 << i)) {
-          sum += arr[i];
-        }
-      }
-      res.push(sum);
+    let k = Math.floor(Math.log(t) / Math.log(x));
+    let p = Math.pow(x, k);
+
+    if (t === p) {
+      memo.set(t, k);
+      return k;
     }
 
-    return res;
+    // option 1: use k times
+    let res = dfs(t - p) + k;
+
+    // option 2: overshoot
+    let res2 = Infinity;
+    if (p * x - t < t) {
+      res2 = dfs(p * x - t) + k + 1;
+    }
+
+    let ans = Math.min(res, res2) + 1;
+    memo.set(t, ans);
+    return ans;
   };
 
-  let leftSums = getSums(left);
-  let rightSums = getSums(right);
-
-  rightSums.sort((a, b) => a - b);
-
-  let ans = Infinity;
-
-  for (let l of leftSums) {
-    let target = goal - l;
-
-    // binary search
-    let lo = 0,
-      hi = rightSums.length - 1;
-
-    while (lo <= hi) {
-      let mid = Math.floor((lo + hi) / 2);
-
-      let sum = l + rightSums[mid];
-      ans = Math.min(ans, Math.abs(sum - goal));
-
-      if (rightSums[mid] < target) {
-        lo = mid + 1;
-      } else {
-        hi = mid - 1;
-      }
-    }
-  }
-
-  return ans;
+  return dfs(target) - 1;
 };
