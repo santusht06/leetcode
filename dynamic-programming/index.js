76435c475b6bfed64a2f9ff8d2580a99f8fcc601
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-24 22:25:00 +0530

    DAY 9

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,12 +1,20 @@
 /**
- * @param {number} a
- * @param {number} b
- * @param {number} c
- * @return {number}
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
  */
-var maximumScore = function (a, b, c) {
-  let sum = a + b + c;
-  let max = Math.max(a, b, c);
+var largestMerge = function (word1, word2) {
+  let res = "";
 
-  return Math.min(sum - max, Math.floor(sum / 2));
+  while (word1.length && word2.length) {
+    if (word1 > word2) {
+      res += word1[0];
+      word1 = word1.slice(1);
+    } else {
+      res += word2[0];
+      word2 = word2.slice(1);
+    }
+  }
+
+  return res + word1 + word2;
 };
