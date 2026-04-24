3c5a205ee481304787f12645c43f8cdff9e996a3
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-24 22:25:32 +0530

    DAY 9

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,20 +1,59 @@
 /**
- * @param {string} word1
- * @param {string} word2
- * @return {string}
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
  */
-var largestMerge = function (word1, word2) {
-  let res = "";
+var minAbsDifference = function (nums, goal) {
+  let n = nums.length;
+  let mid = Math.floor(n / 2);
 
-  while (word1.length && word2.length) {
-    if (word1 > word2) {
-      res += word1[0];
-      word1 = word1.slice(1);
-    } else {
-      res += word2[0];
-      word2 = word2.slice(1);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid);
+
+  const getSums = (arr) => {
+    let res = [];
+    let m = arr.length;
+
+    for (let mask = 0; mask < 1 << m; mask++) {
+      let sum = 0;
+      for (let i = 0; i < m; i++) {
+        if (mask & (1 << i)) {
+          sum += arr[i];
+        }
+      }
+      res.push(sum);
+    }
+
+    return res;
+  };
+
+  let leftSums = getSums(left);
+  let rightSums = getSums(right);
+
+  rightSums.sort((a, b) => a - b);
+
+  let ans = Infinity;
+
+  for (let l of leftSums) {
+    let target = goal - l;
+
+    // binary search
+    let lo = 0,
+      hi = rightSums.length - 1;
+
+    while (lo <= hi) {
+      let mid = Math.floor((lo + hi) / 2);
+
+      let sum = l + rightSums[mid];
+      ans = Math.min(ans, Math.abs(sum - goal));
+
+      if (rightSums[mid] < target) {
+        lo = mid + 1;
+      } else {
+        hi = mid - 1;
+      }
     }
   }
 
-  return res + word1 + word2;
+  return ans;
 };
