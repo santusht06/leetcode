2d85230e32d6b8a1a14db3e4d6a1aab4c1680fa9
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-27 23:32:32 +0530

    DAY 11

--- arrays/index.js
+++ arrays/index.js
@@ -1,13 +1,33 @@
 /**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
  */
-var intersect = function (nums1, nums2) {
-  let ans = nums1.filter((item) => nums2.includes(item));
-  return ans;
+var insert = function (intervals, newInterval) {
+  let result = [];
+  let i = 0;
+  let n = intervals.length;
+
+  // 1️⃣ before overlap
+  while (i < n && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  // 2️⃣ merge overlap
+  while (i < n && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+    i++;
+  }
+
+  result.push(newInterval);
+
+  // 3️⃣ after overlap
+  while (i < n) {
+    result.push(intervals[i]);
+    i++;
+  }
+
+  return result;
 };
-
-((nums1 = [1, 2, 2, 1]), (nums2 = [2]));
-
-console.log(intersect(nums1, nums2));
