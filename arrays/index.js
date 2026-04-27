1e73b59b5331f700f4d1fe66e4365734779f2668
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-27 23:40:02 +0530

    DAY 11

--- arrays/index.js
+++ arrays/index.js
@@ -1,16 +1,34 @@
 /**
- * @param {number[]} nums
- * @param {number[]} moveFrom
- * @param {number[]} moveTo
- * @return {number[]}
+ * @param {number[][]} grid
+ * @return {boolean}
  */
-var relocateMarbles = function (nums, moveFrom, moveTo) {
-  let set = new Set(nums);
+var checkValidGrid = function (grid) {
+  let n = grid.length;
 
-  for (let i = 0; i < moveFrom.length; i++) {
-    set.delete(moveFrom[i]); // remove old position
-    set.add(moveTo[i]); // add new position
+  // step → position
+  let pos = new Array(n * n);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      pos[grid[i][j]] = [i, j];
+    }
   }
 
-  return Array.from(set).sort((a, b) => a - b);
+  // must start at top-left
+  if (pos[0][0] !== 0 || pos[0][1] !== 0) return false;
+
+  // check moves
+  for (let k = 1; k < n * n; k++) {
+    let [r1, c1] = pos[k - 1];
+    let [r2, c2] = pos[k];
+
+    let dr = Math.abs(r1 - r2);
+    let dc = Math.abs(c1 - c2);
+
+    if (!((dr === 2 && dc === 1) || (dr === 1 && dc === 2))) {
+      return false;
+    }
+  }
+
+  return true;
 };
