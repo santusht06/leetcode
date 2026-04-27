a3274db29c248a000f53bf3a2079f6a7c65da52e
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-27 23:34:16 +0530

    DAY 11

--- arrays/index.js
+++ arrays/index.js
@@ -1,21 +1,14 @@
 /**
- * @param {number[]} stones
- * @return {boolean}
+ * @param {number[]} nums
+ * @return {number}
  */
-var stoneGameIX = function (stones) {
-  let count = [0, 0, 0];
+var minMoves = function (nums) {
+  let min = Math.min(...nums);
+  let moves = 0;
 
-  for (let s of stones) {
-    count[s % 3]++;
+  for (let num of nums) {
+    moves += num - min;
   }
 
-  let c0 = count[0];
-  let c1 = count[1];
-  let c2 = count[2];
-
-  if (c0 % 2 === 0) {
-    return c1 > 0 && c2 > 0;
-  } else {
-    return Math.abs(c1 - c2) > 2;
-  }
+  return moves;
 };
