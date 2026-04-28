0dff20cfc8b49ac1f1b08a546020f395e8c72dd9
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:22:45 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,11 +1,36 @@
-SELECT student_id, subject, first_score, latest_score
-FROM (
-    SELECT *,
-        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS first_score,
-        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score,
-        COUNT(*) OVER (PARTITION BY student_id, subject) AS cnt
-    FROM Scores
-) t
-WHERE cnt >= 2 AND latest_score > first_score
-GROUP BY student_id, subject
-ORDER BY student_id, subject;
+SELECT ip, COUNT(*) AS invalid_count
+FROM logs
+WHERE
+    -- not exactly 3 dots → not 4 parts
+    ip NOT REGEXP '^([0-9]+\\.){3}[0-9]+$'
+    
+    OR
+    
+    -- any octet > 255 OR leading zero
+    (
+        -- extract parts
+        (
+            CAST(SUBSTRING_INDEX(ip, '.', 1) AS UNSIGNED) > 255
+            OR (LENGTH(SUBSTRING_INDEX(ip, '.', 1)) > 1 AND LEFT(SUBSTRING_INDEX(ip, '.', 1),1) = '0')
+        )
+        OR
+        (
+            CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) AS UNSIGNED) > 255
+            OR (LENGTH(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1)) > 1 
+                AND LEFT(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1),1) = '0')
+        )
+        OR
+        (
+            CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) AS UNSIGNED) > 255
+            OR (LENGTH(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1)) > 1 
+                AND LEFT(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1),1) = '0')
+        )
+        OR
+        (
+            CAST(SUBSTRING_INDEX(ip, '.', -1) AS UNSIGNED) > 255
+            OR (LENGTH(SUBSTRING_INDEX(ip, '.', -1)) > 1 
+                AND LEFT(SUBSTRING_INDEX(ip, '.', -1),1) = '0')
+        )
+    )
+GROUP BY ip
+ORDER BY invalid_count DESC, ip DESC;
