1d92d8ebecf6812a27ab1fad010daf13adb5ed24
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:22:07 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,4 +1,11 @@
-SELECT user_id, email
-FROM Users
-WHERE email REGEXP '^[a-zA-Z0-9_]+@[a-zA-Z]+\\.com$'
-ORDER BY user_id;
+SELECT student_id, subject, first_score, latest_score
+FROM (
+    SELECT *,
+        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date) AS first_score,
+        FIRST_VALUE(score) OVER (PARTITION BY student_id, subject ORDER BY exam_date DESC) AS latest_score,
+        COUNT(*) OVER (PARTITION BY student_id, subject) AS cnt
+    FROM Scores
+) t
+WHERE cnt >= 2 AND latest_score > first_score
+GROUP BY student_id, subject
+ORDER BY student_id, subject;
