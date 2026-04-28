2b1b7687e2d4485300f5e8ffbfb434ebfcfb86a9
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:26:05 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,60 +1,11 @@
-WITH RECURSIVE hierarchy AS (
-    -- Step 1: build levels
-    SELECT 
-        employee_id,
-        employee_name,
-        manager_id,
-        salary,
-        1 AS level
-    FROM Employees
-    WHERE manager_id IS NULL
-
-    UNION ALL
-
-    SELECT 
-        e.employee_id,
-        e.employee_name,
-        e.manager_id,
-        e.salary,
-        h.level + 1
-    FROM Employees e
-    JOIN hierarchy h ON e.manager_id = h.employee_id
-),
-
-subtree AS (
-    -- Step 2: build manager -> all descendants mapping
-    SELECT 
-        employee_id AS manager,
-        employee_id AS emp
-    FROM Employees
-
-    UNION ALL
-
-    SELECT 
-        s.manager,
-        e.employee_id
-    FROM subtree s
-    JOIN Employees e ON e.manager_id = s.emp
-)
-
 SELECT 
-    h.employee_id,
-    h.employee_name,
-    h.level,
-
-    -- team size (exclude self)
-    COUNT(s.emp) - 1 AS team_size,
-
-    -- total salary (include self)
-    SUM(e.salary) AS budget
-
-FROM hierarchy h
-JOIN subtree s ON h.employee_id = s.manager
-JOIN Employees e ON s.emp = e.employee_id
-
-GROUP BY h.employee_id, h.employee_name, h.level
-
-ORDER BY 
-    h.level ASC,
-    budget DESC,
-    h.employee_name ASC;
+    user_id,
+    ROUND(AVG(CASE WHEN activity_type = 'free_trial' THEN activity_duration END), 2) AS trial_avg_duration,
+    ROUND(AVG(CASE WHEN activity_type = 'paid' THEN activity_duration END), 2) AS paid_avg_duration
+FROM UserActivity
+GROUP BY user_id
+HAVING 
+    SUM(activity_type = 'free_trial') > 0
+    AND
+    SUM(activity_type = 'paid') > 0
+ORDER BY user_id;
