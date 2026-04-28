42cd5f04c8b15ca75c2526ea4ed2a22c9cf6405c
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:23:14 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,36 +1,4 @@
-SELECT ip, COUNT(*) AS invalid_count
-FROM logs
-WHERE
-    -- not exactly 3 dots → not 4 parts
-    ip NOT REGEXP '^([0-9]+\\.){3}[0-9]+$'
-    
-    OR
-    
-    -- any octet > 255 OR leading zero
-    (
-        -- extract parts
-        (
-            CAST(SUBSTRING_INDEX(ip, '.', 1) AS UNSIGNED) > 255
-            OR (LENGTH(SUBSTRING_INDEX(ip, '.', 1)) > 1 AND LEFT(SUBSTRING_INDEX(ip, '.', 1),1) = '0')
-        )
-        OR
-        (
-            CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1) AS UNSIGNED) > 255
-            OR (LENGTH(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1)) > 1 
-                AND LEFT(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 2), '.', -1),1) = '0')
-        )
-        OR
-        (
-            CAST(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1) AS UNSIGNED) > 255
-            OR (LENGTH(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1)) > 1 
-                AND LEFT(SUBSTRING_INDEX(SUBSTRING_INDEX(ip, '.', 3), '.', -1),1) = '0')
-        )
-        OR
-        (
-            CAST(SUBSTRING_INDEX(ip, '.', -1) AS UNSIGNED) > 255
-            OR (LENGTH(SUBSTRING_INDEX(ip, '.', -1)) > 1 
-                AND LEFT(SUBSTRING_INDEX(ip, '.', -1),1) = '0')
-        )
-    )
-GROUP BY ip
-ORDER BY invalid_count DESC, ip DESC;
+SELECT product_id, product_name, description
+FROM products
+WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}'
+ORDER BY product_id;
