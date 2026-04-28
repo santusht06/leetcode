4715d9250953a0ac88dce9c81eebd20ea39234ce
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:23:37 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,4 +1,4 @@
 SELECT product_id, product_name, description
 FROM products
-WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}'
+WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}([^0-9]|$)'
 ORDER BY product_id;
