667ba55e616ca53736961b4b1fe7ffe682e07dde
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:24:27 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,4 +1,31 @@
-SELECT product_id, product_name, description
-FROM products
-WHERE description REGEXP 'SN[0-9]{4}-[0-9]{4}([^0-9]|$)'
-ORDER BY product_id;
+SELECT 
+    sample_id,
+    dna_sequence,
+    species,
+
+    -- starts with ATG
+    CASE 
+        WHEN dna_sequence LIKE 'ATG%' THEN 1 
+        ELSE 0 
+    END AS has_start,
+
+    -- ends with TAA, TAG, or TGA
+    CASE 
+        WHEN dna_sequence REGEXP '(TAA|TAG|TGA)$' THEN 1 
+        ELSE 0 
+    END AS has_stop,
+
+    -- contains ATAT
+    CASE 
+        WHEN dna_sequence LIKE '%ATAT%' THEN 1 
+        ELSE 0 
+    END AS has_atat,
+
+    -- contains at least 3 consecutive G
+    CASE 
+        WHEN dna_sequence REGEXP 'G{3,}' THEN 1 
+        ELSE 0 
+    END AS has_ggg
+
+FROM Samples
+ORDER BY sample_id;
