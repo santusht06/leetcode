7b8de4436a55df194feb745586b451700dde7333
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-28 22:20:29 +0530

    DAY 12

--- database/d1.sql
+++ database/d1.sql
@@ -1,10 +1,5 @@
-SELECT 
-    p.product_name,
-    SUM(o.unit) AS unit
-FROM Orders o
-JOIN Products p 
-    ON o.product_id = p.product_id
-WHERE o.order_date >= '2020-02-01'
-  AND o.order_date < '2020-03-01'
-GROUP BY p.product_name
-HAVING SUM(o.unit) >= 100;
+SELECT e.name, b.bonus
+FROM Employee e
+LEFT JOIN Bonus b
+ON e.empId = b.empId
+WHERE b.bonus < 1000 OR b.bonus IS NULL;
