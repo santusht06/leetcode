2afff06ac29663a17a61d5bdef7a60004d02d479
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-30 22:53:50 +0530

    DAY 13

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,49 +1,16 @@
-/**
- * @param {TreeNode} root
- * @param {number} start
- * @return {number}
- */
-var amountOfTime = function (root, start) {
-  let graph = new Map();
+var canJump = function (nums) {
+  let maxReach = 0;
 
-  // 🔹 build graph
-  const build = (node, parent) => {
-    if (!node) return;
+  for (let i = 0; i < nums.length; i++) {
+    // ❌ can't even reach this index
+    if (i > maxReach) return false;
 
-    if (!graph.has(node.val)) graph.set(node.val, []);
+    // update farthest reach
+    maxReach = Math.max(maxReach, i + nums[i]);
 
-    if (parent) {
-      graph.get(node.val).push(parent.val);
-      graph.get(parent.val).push(node.val);
-    }
-
-    build(node.left, node);
-    build(node.right, node);
-  };
-
-  build(root, null);
-
-  // 🔹 BFS
-  let queue = [start];
-  let visited = new Set([start]);
-  let time = -1;
-
-  while (queue.length) {
-    let size = queue.length;
-
-    for (let i = 0; i < size; i++) {
-      let node = queue.shift();
-
-      for (let nei of graph.get(node)) {
-        if (!visited.has(nei)) {
-          visited.add(nei);
-          queue.push(nei);
-        }
-      }
-    }
-
-    time++;
+    // ✅ early success
+    if (maxReach >= nums.length - 1) return true;
   }
 
-  return time;
+  return true;
 };
