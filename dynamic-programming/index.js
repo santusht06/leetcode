2721b3f454554cb59e3dace1d7f8b64fa2a89515
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-30 23:04:58 +0530

    DAY 13

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,117 +1,63 @@
-var getNumberOfBacklogOrders = function (orders) {
-  const MOD = 1e9 + 7;
-
-  class Heap {
-    constructor(compare) {
-      this.data = [];
-      this.compare = compare;
-    }
-
-    size() {
-      return this.data.length;
-    }
-
-    peek() {
-      return this.data[0];
-    }
-
-    push(val) {
-      this.data.push(val);
-      this._up();
-    }
-
-    pop() {
-      if (this.size() === 1) return this.data.pop();
-
-      const top = this.data[0];
-      this.data[0] = this.data.pop();
-      this._down();
-      return top;
-    }
-
-    _up() {
-      let i = this.size() - 1;
-      while (i > 0) {
-        let p = Math.floor((i - 1) / 2);
-        if (this.compare(this.data[i], this.data[p])) {
-          [this.data[i], this.data[p]] = [this.data[p], this.data[i]];
-          i = p;
-        } else break;
-      }
-    }
-
-    _down() {
-      let i = 0;
-      let n = this.size();
-
-      while (true) {
-        let left = 2 * i + 1;
-        let right = 2 * i + 2;
-        let best = i;
-
-        if (left < n && this.compare(this.data[left], this.data[best])) {
-          best = left;
-        }
-        if (right < n && this.compare(this.data[right], this.data[best])) {
-          best = right;
-        }
-
-        if (best !== i) {
-          [this.data[i], this.data[best]] = [this.data[best], this.data[i]];
-          i = best;
-        } else break;
-      }
+var countPairs = function (nums, low, high) {
+  class TrieNode {
+    constructor() {
+      this.children = [null, null];
+      this.count = 0;
     }
   }
 
-  // max heap (buy)
-  let buy = new Heap((a, b) => a[0] > b[0]);
+  class Trie {
+    constructor() {
+      this.root = new TrieNode();
+    }
 
-  // min heap (sell)
-  let sell = new Heap((a, b) => a[0] < b[0]);
+    insert(num) {
+      let node = this.root;
+      for (let i = 14; i >= 0; i--) {
+        let bit = (num >> i) & 1;
+        if (!node.children[bit]) {
+          node.children[bit] = new TrieNode();
+        }
+        node = node.children[bit];
+        node.count++;
+      }
+    }
 
-  for (let [price, amount, type] of orders) {
-    if (type === 0) {
-      // BUY
-      while (amount > 0 && sell.size() && sell.peek()[0] <= price) {
-        let [sPrice, sAmount] = sell.pop();
+    countLessThan(num, limit) {
+      let node = this.root;
+      let res = 0;
 
-        let used = Math.min(amount, sAmount);
-        amount -= used;
-        sAmount -= used;
+      for (let i = 14; i >= 0; i--) {
+        if (!node) break;
 
-        if (sAmount > 0) {
-          sell.push([sPrice, sAmount]);
+        let bit = (num >> i) & 1;
+        let limBit = (limit >> i) & 1;
+
+        if (limBit === 1) {
+          if (node.children[bit]) {
+            res += node.children[bit].count;
+          }
+          node = node.children[bit ^ 1];
+        } else {
+          node = node.children[bit];
         }
       }
 
-      if (amount > 0) {
-        buy.push([price, amount]);
-      }
-    } else {
-      // SELL
-      while (amount > 0 && buy.size() && buy.peek()[0] >= price) {
-        let [bPrice, bAmount] = buy.pop();
-
-        let used = Math.min(amount, bAmount);
-        amount -= used;
-        bAmount -= used;
-
-        if (bAmount > 0) {
-          buy.push([bPrice, bAmount]);
-        }
-      }
-
-      if (amount > 0) {
-        sell.push([price, amount]);
-      }
+      return res;
     }
   }
 
-  let total = 0;
+  const solve = (limit) => {
+    let trie = new Trie();
+    let count = 0;
 
-  for (let [_, amt] of buy.data) total = (total + amt) % MOD;
-  for (let [_, amt] of sell.data) total = (total + amt) % MOD;
+    for (let num of nums) {
+      count += trie.countLessThan(num, limit);
+      trie.insert(num);
+    }
 
-  return total;
+    return count;
+  };
+
+  return solve(high + 1) - solve(low);
 };
