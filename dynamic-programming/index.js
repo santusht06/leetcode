afcbc2bb5a7b9376a6d7cab230917b099420afac
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-04-30 22:56:21 +0530

    DAY 13

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,30 +1,59 @@
-var splitArray = function (nums) {
-  const n = nums.length;
+var countIslands = function (grid, k) {
+  const m = grid.length;
+  const n = grid[0].length;
 
-  // Step 1: Sieve to find primes
-  let isPrime = new Array(n).fill(true);
-  isPrime[0] = false;
-  if (n > 1) isPrime[1] = false;
+  let visited = Array.from({ length: m }, () => Array(n).fill(false));
 
-  for (let i = 2; i * i < n; i++) {
-    if (isPrime[i]) {
-      for (let j = i * i; j < n; j += i) {
-        isPrime[j] = false;
+  const directions = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+
+  function dfs(r, c) {
+    let stack = [[r, c]];
+    visited[r][c] = true;
+    let sum = 0;
+
+    while (stack.length) {
+      let [x, y] = stack.pop();
+      sum += grid[x][y];
+
+      for (let [dx, dy] of directions) {
+        let nx = x + dx;
+        let ny = y + dy;
+
+        if (
+          nx >= 0 &&
+          ny >= 0 &&
+          nx < m &&
+          ny < n &&
+          grid[nx][ny] > 0 &&
+          !visited[nx][ny]
+        ) {
+          visited[nx][ny] = true;
+          stack.push([nx, ny]);
+        }
+      }
+    }
+
+    return sum;
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] > 0 && !visited[i][j]) {
+        let islandSum = dfs(i, j);
+
+        if (islandSum % k === 0) {
+          count++;
+        }
       }
     }
   }
 
-  // Step 2: Split sums
-  let sumA = 0,
-    sumB = 0;
-
-  for (let i = 0; i < n; i++) {
-    if (isPrime[i]) {
-      sumA += nums[i];
-    } else {
-      sumB += nums[i];
-    }
-  }
-
-  return Math.abs(sumA - sumB);
+  return count;
 };
