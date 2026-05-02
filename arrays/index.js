3c4b0828be91f47ca98c7251f0dbbdd025f79886
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-02 22:25:59 +0530

    DAY 15

--- arrays/index.js
+++ arrays/index.js
@@ -1,42 +1,26 @@
-var numSmallerByFrequency = function (queries, words) {
-  function f(s) {
-    let minChar = "z";
-    let count = 0;
+var canMakePaliQueries = function (s, queries) {
+  let n = s.length;
+  let prefix = new Array(n + 1).fill(0);
 
-    for (let ch of s) {
-      if (ch < minChar) {
-        minChar = ch;
-        count = 1;
-      } else if (ch === minChar) {
-        count++;
-      }
-    }
-
-    return count;
-  }
-
-  let freqWords = words.map(f).sort((a, b) => a - b);
-
-  function upperBound(arr, target) {
-    let left = 0,
-      right = arr.length;
-
-    while (left < right) {
-      let mid = Math.floor((left + right) / 2);
-      if (arr[mid] <= target) left = mid + 1;
-      else right = mid;
-    }
-
-    return left;
+  // build prefix bitmask
+  for (let i = 0; i < n; i++) {
+    let bit = 1 << (s[i].charCodeAt(0) - 97);
+    prefix[i + 1] = prefix[i] ^ bit;
   }
 
   let result = [];
 
-  for (let q of queries) {
-    let fq = f(q);
-    let idx = upperBound(freqWords, fq);
+  for (let [l, r, k] of queries) {
+    let mask = prefix[r + 1] ^ prefix[l];
 
-    result.push(freqWords.length - idx);
+    // count number of 1s (odd chars)
+    let odd = 0;
+    while (mask) {
+      mask &= mask - 1; // remove lowest set bit
+      odd++;
+    }
+
+    result.push(Math.floor(odd / 2) <= k);
   }
 
   return result;
