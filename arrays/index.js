4e839c600e0474a7aca75d330598d369f4cd0e23
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-02 22:29:13 +0530

    DAY 15

--- arrays/index.js
+++ arrays/index.js
@@ -1,39 +1,14 @@
-var longestDiverseString = function (a, b, c) {
+var stringMatching = function (words) {
   let result = [];
 
-  let arr = [
-    ["a", a],
-    ["b", b],
-    ["c", c],
-  ];
-
-  while (true) {
-    // sort by remaining count (descending)
-    arr.sort((x, y) => y[1] - x[1]);
-
-    let added = false;
-
-    for (let i = 0; i < 3; i++) {
-      let [ch, count] = arr[i];
-
-      if (count === 0) continue;
-
-      let n = result.length;
-
-      // check last 2 chars
-      if (n >= 2 && result[n - 1] === ch && result[n - 2] === ch) {
-        continue;
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (i !== j && words[j].includes(words[i])) {
+        result.push(words[i]);
+        break;
       }
-
-      // use this character
-      result.push(ch);
-      arr[i][1]--;
-      added = true;
-      break;
     }
-
-    if (!added) break;
   }
 
-  return result.join("");
+  return result;
 };
