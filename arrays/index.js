6a9eccc8c4fd6851e19c14de7006f50034098c7a
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-02 22:22:45 +0530

    DAY 15

--- arrays/index.js
+++ arrays/index.js
@@ -1,14 +1,23 @@
-var countAsterisks = function (s) {
-  let count = 0;
-  let inside = false;
+var decodeMessage = function (key, message) {
+  let map = {};
+  let curr = "a";
 
-  for (let ch of s) {
-    if (ch === "|") {
-      inside = !inside; // toggle state
-    } else if (ch === "*" && !inside) {
-      count++;
+  for (let ch of key) {
+    if (ch !== " " && map[ch] === undefined) {
+      map[ch] = curr;
+      curr = String.fromCharCode(curr.charCodeAt(0) + 1);
     }
   }
 
-  return count;
+  let result = "";
+
+  for (let ch of message) {
+    if (ch === " ") {
+      result += " ";
+    } else {
+      result += map[ch];
+    }
+  }
+
+  return result;
 };
