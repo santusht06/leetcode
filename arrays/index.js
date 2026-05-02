f66ffe2fd94bd59c6aee1c9c0971b46920fea7c2
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-02 22:27:37 +0530

    DAY 15

--- arrays/index.js
+++ arrays/index.js
@@ -1,44 +1,21 @@
-var findNumOfValidWords = function (words, puzzles) {
-  function getMask(word) {
-    let mask = 0;
-    for (let ch of new Set(word)) {
-      mask |= 1 << (ch.charCodeAt(0) - 97);
+var numSteps = function (s) {
+  let steps = 0;
+  let carry = 0;
+
+  // traverse from right to left (skip first bit)
+  for (let i = s.length - 1; i > 0; i--) {
+    let bit = Number(s[i]) + carry;
+
+    if (bit === 1) {
+      // odd → +1 then /2
+      steps += 2;
+      carry = 1;
+    } else {
+      // even → just /2
+      steps += 1;
     }
-    return mask;
   }
 
-  let freq = new Map();
-
-  for (let word of words) {
-    let mask = getMask(word);
-    if (mask.toString(2).split("1").length - 1 <= 7) {
-      freq.set(mask, (freq.get(mask) || 0) + 1);
-    }
-  }
-
-  let result = [];
-
-  for (let puzzle of puzzles) {
-    let first = 1 << (puzzle[0].charCodeAt(0) - 97);
-
-    let mask = 0;
-    for (let ch of puzzle) {
-      mask |= 1 << (ch.charCodeAt(0) - 97);
-    }
-
-    let sub = mask;
-    let count = 0;
-
-    while (sub) {
-      // must include first letter
-      if ((sub & first) !== 0 && freq.has(sub)) {
-        count += freq.get(sub);
-      }
-      sub = (sub - 1) & mask;
-    }
-
-    result.push(count);
-  }
-
-  return result;
+  // if carry remains at MSB
+  return steps + carry;
 };
