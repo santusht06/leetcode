a3061b243a6ce4076e747ed8d296f44d63817194
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-03 09:28:28 +0530

    DAY 15

--- arrays/index.js
+++ arrays/index.js
@@ -1,27 +1,56 @@
-var numberOfArrays = function (s, k) {
-  const MOD = 1e9 + 7;
-  let n = s.length;
+var minCost = function (nums, queries) {
+  let n = nums.length;
 
-  let memo = new Array(n).fill(-1);
+  let closest = new Array(n);
 
-  function dfs(i) {
-    if (i === n) return 1;
-    if (s[i] === "0") return 0; // no leading zero
-    if (memo[i] !== -1) return memo[i];
-
-    let num = 0;
-    let ways = 0;
-
-    for (let j = i; j < n; j++) {
-      num = num * 10 + (s[j] - "0");
-
-      if (num > k) break;
-
-      ways = (ways + dfs(j + 1)) % MOD;
+  for (let i = 0; i < n; i++) {
+    if (i === 0) closest[i] = 1;
+    else if (i === n - 1) closest[i] = n - 2;
+    else {
+      let left = nums[i] - nums[i - 1];
+      let right = nums[i + 1] - nums[i];
+      closest[i] = left <= right ? i - 1 : i + 1;
     }
-
-    return (memo[i] = ways);
   }
 
-  return dfs(0);
+  let forward = new Array(n - 1);
+  let backward = new Array(n - 1);
+
+  for (let i = 0; i < n - 1; i++) {
+    let diff = nums[i + 1] - nums[i];
+
+    forward[i] = closest[i] === i + 1 ? 1 : diff;
+    backward[i] = closest[i + 1] === i ? 1 : diff;
+  }
+
+  let prefF = [0];
+  for (let i = 0; i < n - 1; i++) {
+    prefF.push(prefF[i] + forward[i]);
+  }
+
+  let prefB = [0];
+  for (let i = 0; i < n - 1; i++) {
+    prefB.push(prefB[i] + backward[i]);
+  }
+
+  let res = [];
+
+  for (let [l, r] of queries) {
+    if (l < r) {
+      res.push(prefF[r] - prefF[l]);
+    } else {
+      res.push(prefB[l] - prefB[r]);
+    }
+  }
+
+  return res;
 };
+
+((nums = [-5, -2, 3]),
+  (queries = [
+    [0, 2],
+    [2, 0],
+    [1, 2],
+  ]));
+
+console.log(minCost(nums, queries));
