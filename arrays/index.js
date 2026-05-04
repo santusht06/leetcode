5dcfb297768e603338f4d2c1b6589af616f00f3a
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-04 21:58:49 +0530

    DAY 16

--- arrays/index.js
+++ arrays/index.js
@@ -1,56 +1,33 @@
-var minCost = function (nums, queries) {
-  let n = nums.length;
+var findItinerary = function (tickets) {
+  const graph = new Map();
 
-  let closest = new Array(n);
+  // Step 1: Build graph
+  for (let [from, to] of tickets) {
+    if (!graph.has(from)) graph.set(from, []);
+    graph.get(from).push(to);
+  }
 
-  for (let i = 0; i < n; i++) {
-    if (i === 0) closest[i] = 1;
-    else if (i === n - 1) closest[i] = n - 2;
-    else {
-      let left = nums[i] - nums[i - 1];
-      let right = nums[i + 1] - nums[i];
-      closest[i] = left <= right ? i - 1 : i + 1;
+  // Step 2: Sort destinations in reverse lex order
+  for (let [key, list] of graph) {
+    list.sort().reverse();
+  }
+
+  const result = [];
+
+  // Step 3: DFS (Hierholzer)
+  function dfs(node) {
+    const dests = graph.get(node) || [];
+
+    while (dests.length > 0) {
+      const next = dests.pop(); // smallest lex
+      dfs(next);
     }
+
+    result.push(node);
   }
 
-  let forward = new Array(n - 1);
-  let backward = new Array(n - 1);
+  dfs("JFK");
 
-  for (let i = 0; i < n - 1; i++) {
-    let diff = nums[i + 1] - nums[i];
-
-    forward[i] = closest[i] === i + 1 ? 1 : diff;
-    backward[i] = closest[i + 1] === i ? 1 : diff;
-  }
-
-  let prefF = [0];
-  for (let i = 0; i < n - 1; i++) {
-    prefF.push(prefF[i] + forward[i]);
-  }
-
-  let prefB = [0];
-  for (let i = 0; i < n - 1; i++) {
-    prefB.push(prefB[i] + backward[i]);
-  }
-
-  let res = [];
-
-  for (let [l, r] of queries) {
-    if (l < r) {
-      res.push(prefF[r] - prefF[l]);
-    } else {
-      res.push(prefB[l] - prefB[r]);
-    }
-  }
-
-  return res;
+  // Step 4: Reverse result
+  return result.reverse();
 };
-
-((nums = [-5, -2, 3]),
-  (queries = [
-    [0, 2],
-    [2, 0],
-    [1, 2],
-  ]));
-
-console.log(minCost(nums, queries));
