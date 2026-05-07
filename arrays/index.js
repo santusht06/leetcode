b2777ad244b862163162cd18ed6d8295f89af3ef
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-07 12:38:41 +0530

    SKIPPED

--- arrays/index.js
+++ arrays/index.js
@@ -1,72 +1,23 @@
 /**
- * @param {number[]} robots
- * @param {number[]} distance
- * @param {number[]} walls
+ * @param {number[]} arr
+ * @param {number} k
  * @return {number}
  */
-var maxWalls = function (robots, distance, walls) {
-  let n = robots.length;
+var findKthPositive = function (arr, k) {
+  let current = 1;
+  let i = 0;
 
-  // Store robots with distances
-  let arr = [];
-  for (let i = 0; i < n; i++) {
-    arr.push([robots[i], distance[i]]);
-  }
-
-  // Sort robots
-  arr.sort((a, b) => a[0] - b[0]);
-
-  // Sort walls
-  walls.sort((a, b) => a - b);
-
-  let destroyed = new Set();
-
-  for (let i = 0; i < n; i++) {
-    let pos = arr[i][0];
-    let dist = arr[i][1];
-
-    // Nearest robot boundaries
-    let leftBlock = i > 0 ? arr[i - 1][0] : -Infinity;
-    let rightBlock = i < n - 1 ? arr[i + 1][0] : Infinity;
-
-    // Shoot left
-    let leftStart = Math.max(pos - dist, leftBlock);
-
-    // Shoot right
-    let rightEnd = Math.min(pos + dist, rightBlock);
-
-    let leftCount = 0;
-    let rightCount = 0;
-
-    // Count left walls
-    for (let wall of walls) {
-      if (wall >= leftStart && wall <= pos) {
-        leftCount++;
-      }
-    }
-
-    // Count right walls
-    for (let wall of walls) {
-      if (wall >= pos && wall <= rightEnd) {
-        rightCount++;
-      }
-    }
-
-    // Choose better direction
-    if (leftCount >= rightCount) {
-      for (let wall of walls) {
-        if (wall >= leftStart && wall <= pos) {
-          destroyed.add(wall);
-        }
-      }
+  while (k > 0) {
+    // Number exists in array
+    if (i < arr.length && arr[i] === current) {
+      i++;
     } else {
-      for (let wall of walls) {
-        if (wall >= pos && wall <= rightEnd) {
-          destroyed.add(wall);
-        }
+      k--;
+      if (k === 0) {
+        return current;
       }
     }
-  }
 
-  return destroyed.size;
+    current++;
+  }
 };
