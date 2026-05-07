5c5c5e52a6048b1773e60948f63edab8e6902906
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-07 12:37:31 +0530

    SKIPPED

--- arrays/index.js
+++ arrays/index.js
@@ -1,17 +1,72 @@
 /**
- * @param {number[][]} triangle
+ * @param {number[]} robots
+ * @param {number[]} distance
+ * @param {number[]} walls
  * @return {number}
  */
-var minimumTotal = function (triangle) {
-  // Copy last row
-  let dp = triangle[triangle.length - 1].slice();
+var maxWalls = function (robots, distance, walls) {
+  let n = robots.length;
 
-  // Start from second last row
-  for (let i = triangle.length - 2; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+  // Store robots with distances
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push([robots[i], distance[i]]);
+  }
+
+  // Sort robots
+  arr.sort((a, b) => a[0] - b[0]);
+
+  // Sort walls
+  walls.sort((a, b) => a - b);
+
+  let destroyed = new Set();
+
+  for (let i = 0; i < n; i++) {
+    let pos = arr[i][0];
+    let dist = arr[i][1];
+
+    // Nearest robot boundaries
+    let leftBlock = i > 0 ? arr[i - 1][0] : -Infinity;
+    let rightBlock = i < n - 1 ? arr[i + 1][0] : Infinity;
+
+    // Shoot left
+    let leftStart = Math.max(pos - dist, leftBlock);
+
+    // Shoot right
+    let rightEnd = Math.min(pos + dist, rightBlock);
+
+    let leftCount = 0;
+    let rightCount = 0;
+
+    // Count left walls
+    for (let wall of walls) {
+      if (wall >= leftStart && wall <= pos) {
+        leftCount++;
+      }
+    }
+
+    // Count right walls
+    for (let wall of walls) {
+      if (wall >= pos && wall <= rightEnd) {
+        rightCount++;
+      }
+    }
+
+    // Choose better direction
+    if (leftCount >= rightCount) {
+      for (let wall of walls) {
+        if (wall >= leftStart && wall <= pos) {
+          destroyed.add(wall);
+        }
+      }
+    } else {
+      for (let wall of walls) {
+        if (wall >= pos && wall <= rightEnd) {
+          destroyed.add(wall);
+        }
+      }
     }
   }
 
-  return dp[0];
+  return destroyed.size;
 };
