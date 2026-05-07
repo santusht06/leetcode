1d703f0a7708d1c81a37f96f0c98213ad1a2e236
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-07 12:35:43 +0530

    SKIPPED

--- arrays/index.js
+++ arrays/index.js
@@ -1,29 +1,24 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * @param {number} numRows
+ * @return {number[][]}
  */
+var generate = function (numRows) {
+  let result = [];
 
-/**
- * @param {number[]} nums
- * @return {TreeNode}
- */
-var sortedArrayToBST = function (nums) {
-  function build(left, right) {
-    if (left > right) return null;
+  for (let i = 0; i < numRows; i++) {
+    let row = [];
 
-    let mid = Math.floor((left + right) / 2);
+    for (let j = 0; j <= i; j++) {
+      // First and last element are always 1
+      if (j === 0 || j === i) {
+        row.push(1);
+      } else {
+        row.push(result[i - 1][j - 1] + result[i - 1][j]);
+      }
+    }
 
-    let root = new TreeNode(nums[mid]);
-
-    root.left = build(left, mid - 1);
-    root.right = build(mid + 1, right);
-
-    return root;
+    result.push(row);
   }
 
-  return build(0, nums.length - 1);
+  return result;
 };
