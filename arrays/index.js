d564d586d24ddbbccd8a292f824b8668e3adbc26
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-05-15 23:12:49 +0530

    RATE LIMITING

--- arrays/index.js
+++ arrays/index.js
@@ -1,31 +1,17 @@
-/**
- * @param {number[]} nums
- * @param {number} target
- * @return {number}
- */
-var maxNonOverlapping = function (nums, target) {
-  let set = new Set();
-  set.add(0);
+// load-test.js
 
-  let prefixSum = 0;
-  let count = 0;
+import http from "k6/http";
+import { sleep } from "k6";
 
-  for (let num of nums) {
-    prefixSum += num;
-
-    // Found a subarray with sum = target
-    if (set.has(prefixSum - target)) {
-      count++;
-
-      // Reset for non-overlapping subarrays
-      set.clear();
-      set.add(0);
-
-      prefixSum = 0;
-    } else {
-      set.add(prefixSum);
-    }
-  }
-
-  return count;
+export const options = {
+  vus: 1500000, // virtual users
+  duration: "30s",
 };
+
+export default function () {
+  const res = http.get("https://google.com");
+
+  console.log(res.status);
+
+  sleep(1);
+}
