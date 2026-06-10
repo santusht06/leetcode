3fe4f3fad8d88dbf377bee17b5c150addf451803
santusht06 <115890693+santusht06@users.noreply.github.com>
2026-06-10 16:51:59 +0530

    DAY 31

--- Hashmap/Index.js
+++ Hashmap/Index.js
@@ -1,32 +1,48 @@
-var MinStack = function () {
-  this.stack = [];
-};
+class TaskScheduler {
+  constructor(maxConcurrent) {
+    this.maxConcurrent = maxConcurrent;
+    this.queue = [];
+    this.runningCount = 0;
+  }
 
-/**
- * @param {number} val
- * @return {void}
- */
-MinStack.prototype.push = function (val) {
-  this.stack.push(val);
-};
+  add(task) {
+    return new Promise((resolve, reject) => {
+      this.queue.push(async () => {
+        try {
+          const result = await task();
+          resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      });
 
-/**
- * @return {void}
- */
-MinStack.prototype.pop = function () {
-  this.stack.pop();
-};
+      this.runNext();
+    });
+  }
 
-/**
- * @return {number}
- */
-MinStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
+  runNext() {
+    if (this.runningCount >= this.maxConcurrent || this.queue.length === 0) {
+      return;
+    }
 
-/**
- * @return {number}
- */
-MinStack.prototype.getMin = function () {
-  return Math.min(...this.stack);
-};
+    this.runningCount++;
+    const nextTask = this.queue.shift();
+
+    nextTask().finally(() => {
+      this.runningCount--;
+      this.runNext();
+    });
+  }
+}
+
+// Example Usage:
+const scheduler = new TaskScheduler(2); // Max 2 tasks at a time
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const task1 = () => delay(1000).then(() => console.log("Task 1 Done"));
+const task2 = () => delay(500).then(() => console.log("Task 2 Done"));
+const task3 = () => delay(300).then(() => console.log("Task 3 Done"));
+
+scheduler.add(task1);
+scheduler.add(task2);
+scheduler.add(task3); // Will wait until task 2 completes
