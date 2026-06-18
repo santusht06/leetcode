3f15148b03f6050f79a5ef9bc113b6ea9b2237ce
santusht06 <santushtkotai1221@gmail.com>
2026-06-18 13:00:37 +0530

    DAY 35

--- maths/maths.js
+++ maths/maths.js
@@ -1,10 +1,29 @@
-var maximumProduct = function (nums) {
-  nums.sort((a, b) => a - b);
-
-  let n = nums.length;
-
-  return Math.max(
-    nums[n - 1] * nums[n - 2] * nums[n - 3], // 3 largest
-    nums[0] * nums[1] * nums[n - 1], // 2 smallest + largest
-  );
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function (n) {
+  let count = 0;
+  for (let i = 0; i < n; i++) {
+    if (isPrime(i)) {
+      count++;
+    }
+  }
+  return count;
 };
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  if (num === 2) return true;
+  if (num % 2 === 0) return false;
+
+  const boundary = Math.sqrt(num);
+  for (let i = 3; i <= boundary; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+n = 10;
+
+console.log(countPrimes(n));
