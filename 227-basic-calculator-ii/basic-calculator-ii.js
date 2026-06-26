5e2ac28566114dfdfedcf3602b5db0cac2d85053
Santusht kotai <115890693+santusht06@users.noreply.github.com>
2026-06-26 18:31:41 +0530

    Time: 18 ms (78.85%) | Memory: 62.7 MB (62.02%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function (s) {
+    const stack = [];
+    let num = 0;
+    let sign = "+";
+
+    for (let i = 0; i < s.length; i++) {
+        const ch = s[i];
+
+        if (ch >= "0" && ch <= "9") {
+            num = num * 10 + Number(ch);
+        }
+
+        if ((ch !== " " && (ch < "0" || ch > "9")) || i === s.length - 1) {
+            if (sign === "+") {
+                stack.push(num);
+            } else if (sign === "-") {
+                stack.push(-num);
+            } else if (sign === "*") {
+                stack.push(stack.pop() * num);
+            } else {
+                stack.push(Math.trunc(stack.pop() / num));
+            }
+
+            sign = ch;
+            num = 0;
+        }
+    }
+
+    return stack.reduce((sum, val) => sum + val, 0);
+};
