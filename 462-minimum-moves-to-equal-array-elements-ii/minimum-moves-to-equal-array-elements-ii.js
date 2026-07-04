80dd17750c906f57219d5d3a37dbf2d26514ea11
Santusht kotai <115890693+santusht06@users.noreply.github.com>
2026-07-05 01:10:03 +0530

    Time: 5 ms (76.32%) | Memory: 57.1 MB (42.11%) - LeetSync

---
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves2 = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const median = nums[Math.floor(nums.length / 2)];
+
+    let moves = 0;
+
+    for (const num of nums) {
+        moves += Math.abs(num - median);
+    }
+
+    return moves;
+};
