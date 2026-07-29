08295f81108124a41d7734f7bac116bcb8c3adbe
Santusht kotai <115890693+santusht06@users.noreply.github.com>
2026-07-29 21:01:20 +0530

    Time: 2 ms (65.53%) | Memory: 54.4 MB (94.28%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  let start = 0;
+  let end = numbers.length - 1;
+
+  while (start < end) {
+    let sum = numbers[start] + numbers[end];
+
+    if (sum == target) {
+      return [start + 1, end + 1];
+    } else if (sum < target) {
+      start++;
+    } else end--;
+  }
+};
+
+((numbers = [2, 7, 11, 15]), (target = 9));
+
+console.log(twoSum(numbers, target));
