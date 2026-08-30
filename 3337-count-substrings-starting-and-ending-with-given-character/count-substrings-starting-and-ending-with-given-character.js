2d511b1383afb770d14205fbb29f6be4be2326da
Santusht kotai <115890693+santusht06@users.noreply.github.com>
2026-08-30 13:42:08 +0530

    Time: 4 ms (74.07%) | Memory: 58.2 MB (74.07%) - LeetSync

---
+++ 3337-count-substrings-starting-and-ending-with-given-character/count-substrings-starting-and-ending-with-given-character.js
@@ -0,0 +1,14 @@
+var countSubstrings = function (s, c) {
+  let count = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === c) {
+      count++;
+    }
+  }
+
+  return (count * (count + 1)) / 2;
+};
+
+((s = "zzz"), (c = "z"));
+console.log(countSubstrings(s, c));
